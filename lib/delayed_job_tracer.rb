bb02ab9870586d01c67f74f160a581ad23205a3e
Mike Stewart <mike.stewart@introhive.com>
2016-09-21 16:47:00 -0300

    Add postgres support.

--- lib/delayed_job_tracer.rb
+++ lib/delayed_job_tracer.rb
@@ -4,7 +4,8 @@ else
   require File.dirname(__FILE__) + '/delayed_job_tracer/notifier'
   require File.dirname(__FILE__) + '/delayed_job_tracer/message_finder'
   # require File.dirname(__FILE__) + '/delayed_job_tracer/mysql_interface'
-  
+  require File.dirname(__FILE__) + '/delayed_job_tracer/pg_interface'
+
   class DelayedJobTracer
 
     def self.config
@@ -20,6 +21,7 @@ else
       Notifier.notify_admin_of_email_issue unless MessageFinder.found_recent_message?
       # Notifier.notify_admin_of_queue_issue unless MySQLInterface.delayed_job_queue_ok?
       # MySQLInterface.queue_delayed_job
+      PgInterface.queue_delayed_job
     end
 
   end
---
+++ lib/delayed_job_tracer/pg_interface.rb
@@ -0,0 +1,52 @@
+require 'pg'
+
+class PgInterface
+
+  # Connects to the db and submits a query
+  def self.query(sql)
+    c = DelayedJobTracer.config['database']
+    conn = PG.connect(:host => c['ip'], :dbname => c['database'], :user => c['user'], :password => c['password'], :sslmode => :require)
+    conn.exec(sql)
+  end
+
+  # Returns true if there are no stale records
+  def self.delayed_job_queue_ok?
+    query(delayed_job_stale_records).count.zero?
+  end
+
+  # Inserts a delayed_job record
+  def self.queue_delayed_job
+    query(delayed_job_record).to_s
+  end
+
+  # SQL for selecting stale delayed_job records
+  def self.delayed_job_stale_records
+    c = DelayedJobTracer.config['delayed_job']
+    "SELECT * FROM delayed_jobs WHERE created_at < '#{(Time.now-c['stale']).utc.strftime("%Y-%m-%d %H:%M:%S")}'"
+  end
+
+  # SQL for inserting a delayed_job record
+  def self.delayed_job_record
+    "INSERT INTO delayed_jobs (handler, run_at, created_at, updated_at) VALUES
+      ('#{delayed_job_handler}', '#{pg_timestamp}', '#{pg_timestamp}', '#{pg_timestamp}')"
+  end
+
+  # SQL helper method for inserting a delayed_job record
+  def self.delayed_job_handler
+    "--- !ruby/struct:Delayed::PerformableMailer
+object: !ruby/class DelayedJobTracerMailer
+method_name: :delayed_job_tracer_test_message
+args: []"
+  end
+
+  # Timestamp in the format that e-mails expect
+  def self.email_timestamp
+    Time.now.strftime("%a, %e %b %Y %H:%M:%S %z")
+  end
+
+  # Timestamp in the format that MySQL expects
+  def self.pg_timestamp
+    Time.now.utc.strftime("%Y-%m-%d %H:%M:%S")
+  end
+
+end
