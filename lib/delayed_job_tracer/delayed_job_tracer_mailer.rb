2336e6c2da74179de2624e342df2843af9dc35ca
Mike Stewart <mike.stewart@introhive.com>
2016-09-22 14:18:57 -0300

    Use ERB in the mailer, and the correct subject for search.

--- lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
+++ lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
@@ -1,10 +1,10 @@
 class DelayedJobTracerMailer < ActionMailer::Base
 
   def delayed_job_tracer_test_message
-    config  = YAML.load_file(Rails.root.join('config', 'delayed_job_tracer_config.yml'))
+    config  = YAML.load(ERB.new(File.read(Rails.root.join('config', 'delayed_job_tracer_config.yml'))).result)
     to      = config['monitor']['username']
     from    = ActionMailer::Base.smtp_settings[:user_name]
-    subject = "[DelayedJobTracer] Test message sent at: #{Time.zone.now}"
+    subject = "[#{config['monitor']['subject']}] Test message sent at: #{Time.zone.now}"
     message = 'This is a test message to ensure messages are being delivered successfully via delayed_job.'
     mail(:to => to, :from => from, :subject => subject) do |format|
       format.text { render :text => message }
